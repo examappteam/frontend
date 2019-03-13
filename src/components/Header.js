27027c7e70a67a26bd226a97d4fdec785133cd0d
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-03-13 09:03:13 +0200

    Define css classes as "className"

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ function Header() {
                     <img id="logo" src="images/examapp-logo.png" alt="Examapp"></img>
                 </div>
                 <div className="pure-u-1 pure-u-md-1-3">                
-                    <button id="logout-button" class="pure-button button-error">Logout</button>
-                    <button id="logout-button" class="pure-button button-secondary">Settings</button>
-                    <Link to="/dashboard"><button id="logout-button" class="pure-button pure-button-primary">Dashboard</button></Link>                   
+                    <button id="logout-button" className="pure-button button-error">Logout</button>
+                    <button id="logout-button" className="pure-button button-secondary">Settings</button>
+                    <Link to="/dashboard"><button id="logout-button" className="pure-button pure-button-primary">Dashboard</button></Link>                   
                 </div>
             </div>
         </header>
