a561c414131bd5e0ce57471cf0d0ea4cf92327a0
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-03-15 13:49:41 +0200

    Create a base for the teacher class view page

--- src/components/common/ScrollableListMenu.js
+++ src/components/common/ScrollableListMenu.js
@@ -7,7 +7,14 @@ function ScrollableListMenu(props) {
     console.log("id",props.menuItems[1].id)
     const category = props.category
     const menuItems = props.menuItems.map(
-        item => <ListMenuItem handler={props.handler(category, item.id)}key={item.id} itemName={item.name} itemExtra={item.linkedClass}/>
+        item => <ListMenuItem 
+            handler={props.handler(category, item.id)} 
+            id={item.id} 
+            itemName={item.name} 
+            itemExtra={item.linkedClass}
+            selectedItem={props.selectedItem}
+            categoryId={category}
+            selectedCategory={props.selectedCategory}/>
     )
 
     return(
