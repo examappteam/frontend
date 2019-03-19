21f13126d166bb691ce32cac2f2311a64ff3a510
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-03-19 10:58:38 +0200

    Template to handle different itemExtras

--- src/components/common/ScrollableListMenu.js
+++ src/components/common/ScrollableListMenu.js
@@ -6,16 +6,43 @@ import "./styles/ScrollableListMenuStyle.css"
 function ScrollableListMenu(props) {
     console.log("id",props.menuItems[1].id)
     const category = props.category
-    const menuItems = props.menuItems.map(
-        item => <ListMenuItem 
-            handler={props.handler(category, item.id)} 
-            id={item.id} 
-            itemName={item.name} 
-            itemExtra={item.linkedClass}
-            selectedItem={props.selectedItem}
-            categoryId={category}
-            selectedCategory={props.selectedCategory}/>
-    )
+    let menuItems
+
+    // try to find a better way to do this in the future
+    if(category === 1) {
+        menuItems = props.menuItems.map(
+            item => <ListMenuItem 
+                handler={props.handler(category, item.id)} 
+                id={item.id} 
+                itemName={item.name} 
+                itemExtra={item.linkedClass}    // !!!we should be able to use different item extras
+                selectedItem={props.selectedItem}
+                categoryId={category}
+                selectedCategory={props.selectedCategory}/>
+        )
+    } else if(category === 2) {
+        menuItems = props.menuItems.map(
+            item => <ListMenuItem 
+                handler={props.handler(category, item.id)} 
+                id={item.id} 
+                itemName={item.name} 
+                itemExtra={item.date}
+                selectedItem={props.selectedItem}
+                categoryId={category}
+                selectedCategory={props.selectedCategory}/>
+        )
+    } else {
+        menuItems = props.menuItems.map(
+            item => <ListMenuItem 
+                handler={props.handler(category, item.id)} 
+                id={item.id} 
+                itemName={item.name} 
+                itemExtra=""
+                selectedItem={props.selectedItem}
+                categoryId={category}
+                selectedCategory={props.selectedCategory}/>
+        )
+    }
 
     return(
         <div className="pure-menu pure-menu-scrollable scrollable-list-menu">
