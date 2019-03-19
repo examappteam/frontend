77f382ae178210d9855d5632e6ab9d4756097060
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-03-19 13:46:25 +0200

    Add template to create new class dialog

---
+++ src/components/dashboard/dialogs/CreateNewClassDialog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function CreateNewClassDialog() {
+    return(
+        <div>
+            <h1>HELLOOO THERE</h1>
+        </div>
+    )
+}
+
+export default CreateNewClassDialog
