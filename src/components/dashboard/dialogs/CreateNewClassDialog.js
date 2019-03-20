2790af5c16a488098f1ed828418716c3051dabf9
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-03-20 10:04:47 +0200

    Implement basic dialog for creating new class

--- src/components/dashboard/dialogs/CreateNewClassDialog.js
+++ src/components/dashboard/dialogs/CreateNewClassDialog.js
@@ -1,11 +1,42 @@
-import React from "react"
+import React, {Component} from "react"
 
-function CreateNewClassDialog() {
-    return(
-        <div>
-            <h1>HELLOOO THERE</h1>
-        </div>
-    )
+import "../styles/DashboardStyle.css"
+import "../../common/styles/Modal.css"
+
+class CreateNewClassDialog extends Component {
+    constructor() {
+        super()
+        this.state = {
+            className: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const {name, value} = event.target
+
+        this.setState({
+            [name]: value
+        }) 
+    }
+
+    render() {
+        return(
+            <div className="padded-box">
+                <form className="pure-form pure-form-aligned">
+                    <label>Name of the class: </label>
+                    <input
+                        type="text" 
+                        value={this.state.className} 
+                        name="className" 
+                        placeholder="eg. TVT17SPO" 
+                        onChange={this.handleChange}
+                    />            
+                </form>
+                <button className="pure-button btn-continue">Create class</button>
+            </div>        
+        )
+    }
 }
 
 export default CreateNewClassDialog
