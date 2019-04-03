bd218da432b826e8ca2982549f4efb773f6c5c95
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2019-04-03 11:36:31 +0300

    Remove redundant componentDidMount()

--- src/components/dashboard/TeacherDashboard.js
+++ src/components/dashboard/TeacherDashboard.js
@@ -105,20 +105,6 @@ class TeacherDashboard extends Component {
         console.log("showstate",this.state.showState)
     }
 
-    componentDidMount(){
-        fetch('https://cors-anywhere.herokuapp.com/http://examapp.crenxu.com:22501/main/exam/1', {
-            method: 'GET',
-            headers:{
-                'Content-Type': 'application/json',
-                Authorization: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZWFjaGVyIiwiQVVUSCI6W3siYXV0aG9yaXR5IjoiUk9MRV9URUFDSEVSIn1dLCJpYXQiOjE1NTM2Nzk2MjcsImV4cCI6MTU1Mzc2NjAyN30.YoXboSqq9CnPmU7HNyBmHXQFnZl5N5Nl6g1GclmJefc0ELR3oDpvdnmhfMtUJUWURa3YOzV31xdtJccVlzV6Yg',
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
-    }
-
     render() {
         console.log(this.state.categories[this.state.selectedCategoryId][this.state.selectionId])
         console.log(sessionStorage.getItem('jwtToken'));
