bb85facb9faa16da5c9c157239ce4b59a9209ff2
t7pumi01 <t7pumi01@students.oamk.fi>
2019-04-24 12:13:38 +0300

    examgradingview-update

--- src/components/exam-grading-page/ExamGradingView.js
+++ src/components/exam-grading-page/ExamGradingView.js
@@ -2,8 +2,6 @@ import React, {Component} from "react";
 
 import "../../style.css";
 import "./styles/styles.css";
-//import students from "./exampleData"
-//import questions from "./exampleData"
 import NavBars from "./NavBars"
 import Questions from "./Questions"
 import AnswerForm from "./AnswerForm"
@@ -20,9 +18,7 @@ class ExamGradingView extends Component {
             }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
-        this.handleFirstClick = this.handleFirstClick.bind(this)
-        this.handleSecondClick = this.handleSecondClick.bind(this)
-        this.handleThirdClick = this.handleThirdClick.bind(this)
+        this.handleClick = this.handleClick.bind(this)
 
     }
 
@@ -36,22 +32,10 @@ class ExamGradingView extends Component {
         this.state.value == 0 ? alert("Are you sure you want to give 0 points?") : alert(this.state.value);        event.preventDefault();
     }
 
-    handleFirstClick() {
+    handleClick() {
         this.setState({
             answerExample: "Java uses What Was It compiler to enable high performance. WWI is used to convert the instructions into bytecodes.",
             answerId: 1
-        })
-    }
-    handleSecondClick() {
-        this.setState({
-            answerExample: "Java uses Wonderful Time compiler to enable high performance. WT is used to convert the instructions into bytecodes.",
-            answerId: 2
-        })
-    }
-    handleThirdClick() {
-        this.setState({
-            answerExample: "Java uses Just In Time compiler to enable high performance. JIT is used to convert the instructions into bytecodes.",
-            answerId: 3
         })
     }
 
@@ -65,9 +49,7 @@ class ExamGradingView extends Component {
             <div className="pure-g padded-box">
                 <NavBars 
                     data={this.state}
-                    handleFirstClick={this.handleFirstClick}
-                    handleSecondClick={this.handleSecondClick}
-                    handleThirdClick={this.handleThirdClick}
+                    handleClick={this.handleClick}
                     />
 
                 <div className="pure-u-2-3">
