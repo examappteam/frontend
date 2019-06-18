e1f51af606d21bf69c29dcd7d3b818003089dbac
Michel Schols <michelschols@gmail.com>
2019-06-18 14:06:03 +0200

    Snapshot test

--- src/components/dashboard/TeacherDashboardTest.test.js
+++ src/components/dashboard/TeacherDashboardTest.test.js
@@ -2,15 +2,24 @@ import React from "react";
 import { create } from "react-test-renderer";
 import TeacherDashboard from "./TeacherDashboard";
 
+describe("TeacherDashboard", () => {
+  test("it matches the snapshot", () => {
+    const component = create(<TeacherDashboard />);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
+
 describe("TeacherDashboard enable delete exam button after clicking exam", () => {
   test("the delete button becomes enabled when an exam is clicked", () => {
     
     const componentDashboard = create(<TeacherDashboard/>);
     const rootInstanceDashboard = componentDashboard.root;
-    const teacherDashboard = rootInstanceDashboard.findByType("div");
-   // const deleteExamButton = rootInstanceDashboard.findByProps()
-    // teacherDashboard.getElementById("delete-exam-button");
-    expect(deleteExamButton.props.children).toBe("PROCEED TO CHECKOUT");
+    const teacherDashboard = rootInstanceDashboard.findByType(React.Component);
+    console.log(teacherDashboard.state.selectedCategoryId);
+    
+    // const deleteExamButton = rootInstanceDashboard.findByProps()
+    // // teacherDashboard.getElementById("delete-exam-button");
+    // expect(deleteExamButton.props.children).toBe("PROCEED TO CHECKOUT");
 
     const component = create(<Button text="SUBSCRIBE TO BASIC" />);
     const rootInstance = component.root;
